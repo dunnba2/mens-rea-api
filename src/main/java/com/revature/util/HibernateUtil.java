42f5180ae2c583e50bcd5893a0f9d3ee407a55ef
Blake Dunn <bdunn@DESKTOP-9VJVNCN>
2020-02-07 17:16:40 -0500

    updated HibernateUtil class and added Reflections dependency to pom.xml

--- src/main/java/com/revature/util/HibernateUtil.java
+++ src/main/java/com/revature/util/HibernateUtil.java
@@ -1,4 +1,39 @@
 package com.revature.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.reflections.Reflections;
+
+import javax.persistence.Entity;
+import java.util.Set;
+
 public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    private static SessionFactory buildSessionFactory() {
+
+        try{
+
+            Configuration config = new Configuration();
+            config.configure();
+            assignAnnotatedClasses(config);
+            return config.buildSessionFactory();
+
+        }catch (Exception e) {
+            e.printStackTrace();
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return (sessionFactory == null) ? sessionFactory = buildSessionFactory() :sessionFactory;
+    }
+
+    private static void assignAnnotatedClasses(Configuration config) {
+
+        Reflections reflect = new Reflections("com.revature.models");
+        Set<Class<? extends Object>> entities = reflect.getTypesAnnotatedWith(Entity.class);
+        entities.forEach(config::addAnnotatedClass);
+    }
 }
