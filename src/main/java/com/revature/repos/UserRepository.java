eeae905e8c3aab7d6616c3fd63af085af88f36da
Ivanna Mahabir <iv.mahabir@gmail.com>
2020-02-15 13:46:37 -0500

    git conflicts

---
+++ src/main/java/com/revature/repos/UserRepository.java
@@ -0,0 +1,71 @@
+package com.revature.repos;
+
+
+import com.revature.models.User;
+import com.revature.web.dtos.Credentials;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserRepository implements CrudRepository<User> {
+
+    private SessionFactory factory;
+
+    @Autowired
+    public UserRepository(SessionFactory factory) {
+        super();
+        this.factory = factory;
+    }
+
+    @Override
+    public User save(User newUser) {
+
+        Session session = factory.getCurrentSession();
+        session.save(newUser);
+
+        return newUser;
+    }
+
+    @Override
+    public List<User> findAll() {
+
+        List<User> users = new ArrayList<>();
+        Session session = factory.getCurrentSession();
+        session.createQuery("from User", User.class).getResultList();
+        return users;
+    }
+
+    @Override
+    public User findById(Integer id) {
+
+        Session session = factory.getCurrentSession();
+        return session.get(User.class, id);
+    }
+
+    @Override
+    public void update(User updatedObj) {
+
+        Session session = factory.getCurrentSession();
+        session.saveOrUpdate(updatedObj);
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    public User findByCredentials(Credentials creds) {
+
+        Session session = factory.getCurrentSession();
+        return session.createQuery("from User au where au.username = :un and au.password = :pw", User.class)
+                .setParameter("un", creds.getUsername())
+                .setParameter("pw", creds.getPassword())
+                .getSingleResult();
+    }
+}
--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -2,6 +2,7 @@ package com.revature.services;
 
 import com.revature.models.Role;
 import com.revature.models.User;
+import com.revature.repos.UserRepository;
 import com.revature.web.dtos.Credentials;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
